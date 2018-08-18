3b036e29d427756018a21649a0c52c3478208e55
Jake Woods <jake@jakewoods.net>
2018-08-18 14:44:49 +1000

    nixos/thermald: add manual config file
    thermald has two modes: zero-config and manual. Sometimes it is useful
    to manually configure thermald to achieve better thermal results or to give
    thermald a hand when detecting possible cooling options.

--- nixos/modules/services/hardware/thermald.nix
+++ nixos/modules/services/hardware/thermald.nix
@@ -6,25 +6,50 @@ let
   cfg = config.services.thermald;
 in {
   ###### interface
-  options = { 
-    services.thermald = { 
+  options = {
+    services.thermald = {
       enable = mkOption {
         default = false;
         description = ''
           Whether to enable thermald, the temperature management daemon.
-        ''; 
-      };  
-    };  
-  };  
+        '';
+      };
+
+      debug = mkOption {
+        type = types.bool;
+        default = false;
+        description = ''
+          Whether to enable debug logging.
+        '';
+      };
+
+      configFile = mkOption {
+        type = types.nullOr types.path;
+        default = null;
+        description = "the thermald manual configuration file.";
+      };
+    };
+  };
 
   ###### implementation
   config = mkIf cfg.enable {
     services.dbus.packages = [ pkgs.thermald ];
 
+    environment.etc."thermald/thermal-conf.xml".text =
+      mkIf (cfg.configFile != null) cfg.configFile;
+
     systemd.services.thermald = {
       description = "Thermal Daemon Service";
       wantedBy = [ "multi-user.target" ];
-      script = "exec ${pkgs.thermald}/sbin/thermald --no-daemon --dbus-enable";
+      serviceConfig = {
+        ExecStart = ''
+          ${pkgs.thermald}/sbin/thermald \
+            --no-daemon \
+            ${optionalString cfg.debug "--loglevel=debug"} \
+            ${optionalString (cfg.configFile != null) "--config-file ${cfg.configFile}"} \
+            --dbus-enable
+        '';
+      };
     };
   };
 }
